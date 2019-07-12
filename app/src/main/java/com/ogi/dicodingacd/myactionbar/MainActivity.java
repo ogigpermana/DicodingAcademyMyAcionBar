b20777f3132bc6f2ac455a7b6ffb0fce0e97aa67
Ogi G Permana <hunterdumay@gmail.com>
2019-07-12 08:51:49 +0700

    Implementasi sederhana MenuFragment dan MenuActivity

---
+++ app/src/main/java/com/ogi/dicodingacd/myactionbar/MainActivity.java
@@ -0,0 +1,46 @@
+package com.ogi.dicodingacd.myactionbar;
+
+import android.content.Intent;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menu1:
+                MenuFragment menuFragment = new MenuFragment();
+                FragmentManager mFragmentManager = getSupportFragmentManager();
+                FragmentTransaction mFragmentTransaction = mFragmentManager.beginTransaction();
+                mFragmentTransaction.replace(R.id.fragment_container, menuFragment);
+                mFragmentTransaction.addToBackStack(null);
+                mFragmentTransaction.commit();
+                return true;
+            case R.id.menu2:
+                Intent i = new Intent(this, MenuActivity.class);
+                startActivity(i);
+                return true;
+            default:
+                return true;
+        }
+    }
+}
